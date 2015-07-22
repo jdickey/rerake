da43ebf52f84ef8b265b811373ab45211ab9b49b
Jeff Dickey <jdickey@seven-sigma.com>
2015-07-23 02:49:55 +0800

    Added parser for MiniTest output.
    Given an Array of input strings, searches for a valid MiniTest report detail line; if found, parses its counts into a `counts` attribute Hash.
    
    Tests: 18 tests, 26 assertions, 0 failures, 0 errors, 0 skips
    Coverage: 76/76 LOC (100.0%) covered
    RuboCop: 4 files inspected, no offenses detected
    Flay: Total score 0
    Flog: Total 55.9; method average 4.7; max 8.8 (Rerake::Parser::MiniTest#values)
    Reek: 0 warnings
    Cane: 0 violations total

---
+++ lib/rerake/parser/mini_test.rb
@@ -0,0 +1,58 @@
+
+require 'pry-byebug'
+
+module Rerake
+  module Parser
+    # Parses MiniTest (Spec/Unit) output, storing reported count values.
+    class MiniTest
+      attr_reader :counts
+
+      def initialize(report_lines)
+        @report_lines = report_lines
+        @counts = {}
+        indexes.each { |index| @counts[index] = 0 }
+        self
+      end
+
+      def parse
+        return self unless detail_line
+        assign_counts
+        self
+      end
+
+      private
+
+      attr_reader :report_lines
+
+      def assign_counts
+        values.each_with_index do |value, index|
+          @counts[indexes[index]] = value
+        end
+      end
+
+      def detail_line
+        @detail_line ||= Array(report_lines).grep(matcher).first
+      end
+
+      def matcher
+        pattern_str = detail_line_part_strings.map do |what|
+          '(\d+?) ' + what
+        end.join(', ')[0..-2]
+        Regexp.new pattern_str
+      end
+
+      def detail_line_part_strings
+        ['tests', 'assertions', 'failures', 'errors', 'skips']
+      end
+
+      def indexes
+        detail_line_part_strings.map(&:to_sym)
+      end
+
+      def values
+        match_data = detail_line.match matcher
+        match_data.captures.map(&:to_i)
+      end
+    end # class Rerake::Parser::MiniTest
+  end
+end
